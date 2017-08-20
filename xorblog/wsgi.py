625817c3e04c87daded1dfde550ed99db35e9f66
Alexandr Dorofeyev <xoralex@protonmail.com>
2017-08-20 15:36:41 +0600

    Connect Django to AppEngine

--- xorblog/wsgi.py
+++ xorblog/wsgi.py
@@ -7,7 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+sys.path.append('venv/lib/python2.7/site-packages')
 
 from django.core.wsgi import get_wsgi_application
 
